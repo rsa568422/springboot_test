fd15a03ba8ace2979c1aa7d25b6e7fde966da2ea
Roberto Sánchez Arévalo <rsa568422@hotmail.com>
2022-05-21 01:02:56 +0200

    Clase 55
    Se agregan las clases BancoRepository y CuentaRepository para el acceso a la base de datos.
    
    Se agrega la clase CuentaService para el servicio de la cuenta.

---
+++ src/main/java/org/rsa/test/springboot/app/services/CuentaService.java
@@ -0,0 +1,17 @@
+package org.rsa.test.springboot.app.services;
+
+import org.rsa.test.springboot.app.models.Cuenta;
+
+import java.math.BigDecimal;
+
+public interface CuentaService {
+
+    Cuenta findById(Long id);
+
+    int revisarTotalTransferencias(Long bancoId);
+
+    BigDecimal revisarSaldo(Long cuentaId);
+
+    void transferir(Long numCuentaOrigen, Long numCuentaDestino, BigDecimal monto);
+
+}
