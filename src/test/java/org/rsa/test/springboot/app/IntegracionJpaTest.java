4a5e0036dc8a44bae03b0a9203e31717e38dabce
Roberto Sánchez Arévalo <rsa568422@hotmail.com>
2022-05-23 00:21:29 +0200

    Clase 69
    Se agrega la clase IntegracionJpaTest con ejemplos de test utilizando la base de datos definida para las pruebas.
    
    Esta base de datos es generada al comienzo de las pruebas y eliminada al final, realizando un rollback de las transacciones pendientes entre pruebas, de manera que estas sean pruebas unitarias y no se vean afectadas por la ejecución de las demás.
    
    Con estas pruebas podemos simular una base de datos sobre la que utilizar nuestros repositories, lo cual nos da un entorno controlado y fijo para las pruebas, pudiendo prescindir en estas pruebas de mocks, ya que sabemos qué debe devolver cada método probado.

---
+++ src/test/java/org/rsa/test/springboot/app/IntegracionJpaTest.java
@@ -0,0 +1,53 @@
+package org.rsa.test.springboot.app;
+
+import org.junit.jupiter.api.Test;
+import org.rsa.test.springboot.app.models.Cuenta;
+import org.rsa.test.springboot.app.repositories.CuentaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+public class IntegracionJpaTest {
+
+    @Autowired
+    CuentaRepository cuentaRepository;
+
+    @Test
+    void testFindById() {
+        Optional<Cuenta> cuenta = this.cuentaRepository.findById(1L);
+
+        assertTrue(cuenta.isPresent());
+        assertEquals("Andrés", cuenta.orElseThrow().getPersona());
+    }
+
+    @Test
+    void testFindByPersona() {
+        Optional<Cuenta> cuenta = this.cuentaRepository.findByPersona("Andrés");
+
+        assertTrue(cuenta.isPresent());
+        assertEquals("1000.00", cuenta.orElseThrow().getSaldo().toPlainString());
+    }
+
+    @Test
+    void testFindByPersonaThrowException() {
+        Optional<Cuenta> cuenta = this.cuentaRepository.findByPersona("John");
+
+        assertThrows(NoSuchElementException.class, cuenta::orElseThrow);
+        assertFalse(cuenta.isPresent());
+    }
+
+    @Test
+    void testFindAll() {
+        List<Cuenta> cuentas = this.cuentaRepository.findAll();
+
+        assertFalse(cuentas.isEmpty());
+        assertEquals(2, cuentas.size());
+    }
+
+}
