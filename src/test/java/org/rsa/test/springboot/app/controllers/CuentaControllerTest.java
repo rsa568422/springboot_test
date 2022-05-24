b0c39267b433b8bcc9d324aa57f57c6b5ac3c5ee
Roberto Sánchez Arévalo <rsa568422@hotmail.com>
2022-05-25 00:47:38 +0200

    Clase 78
    Se agrega un atributo del tipo ObjectMapper a la clase CuentaControllerTest y se agrega el método setUp etiquetado con @BeforeEach para inicializarlo antes de los tests.
    
    Se agrega el método testTransferir con un ejemplo de test básico sobre un método POST. Este ejemplo se completará en la siguiente clase.

--- src/test/java/org/rsa/test/springboot/app/controllers/CuentaControllerTest.java
+++ src/test/java/org/rsa/test/springboot/app/controllers/CuentaControllerTest.java
@@ -1,17 +1,24 @@
 package org.rsa.test.springboot.app.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.rsa.test.springboot.app.models.TransaccionDto;
 import org.rsa.test.springboot.app.services.CuentaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.rsa.test.springboot.app.Datos.crearCuenta001;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(CuentaController.class)
@@ -22,6 +29,13 @@ class CuentaControllerTest {
 
     @MockBean
     private CuentaService cuentaService;
+
+    ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp() {
+        this.objectMapper = new ObjectMapper();
+    }
 
     @Test
     void testDetalle() throws Exception {
@@ -35,6 +49,24 @@ class CuentaControllerTest {
                 .andExpect(jsonPath("$.saldo").value("1000"));
 
         verify(this.cuentaService).findById(1L);
+    }
+
+    @Test
+    void testTransferir() throws Exception {
+        TransaccionDto dto = new TransaccionDto();
+        dto.setBancoId(1L);
+        dto.setCuentaOrigenId(1L);
+        dto.setCuentaDestinoId(2L);
+        dto.setMonto(new BigDecimal("100"));
+
+        this.mvc.perform(post("/api/cuentas/transferir")
+                .contentType(APPLICATION_JSON)
+                .content(this.objectMapper.writeValueAsString(dto)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(APPLICATION_JSON))
+                .andExpect(jsonPath("$.date").value(LocalDate.now().toString()))
+                .andExpect(jsonPath("$.mensaje").value("Transferencia realizada con éxito"))
+                .andExpect(jsonPath("$.transaccion.cuentaOrigenId").value(dto.getCuentaOrigenId()));
     }
 
 }
